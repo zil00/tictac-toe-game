46347cda2b577d73d4a0e1d8f96807d9ce62dee0
Sharzil <sharzil362@gmail.com>
2025-11-08 23:14:47 -0600

    changed the name of tilemap.gd to controller.gd

--- controller.gd
+++ controller.gd
@@ -1,16 +1,23 @@
-class_name Controller
 extends Node2D
 
-var model:Model
-var view:View
-
+var board_size:int
+## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	view = $TileMapLayer
-	view.connect("cell_selected", tile_selected)
-	model = Model.new(5, view)
-	view.set_model(model)
-	model.reset_board()
-	
-	
-func tile_selected(coord:Vector2i) -> void:
-	model.toggle_tile(coord)
+	#var used_rect = $TileMapLayer.get_used_rect().size.x
+	pass
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var click_global_pos = event.position
+			#converts the global click into the tilemaps local position
+			var click_map_pos = $TileMapLayer.local_to_map($TileMapLayer.to_local(click_global_pos))
+			#the total map layer area
+			var used_rect = $TileMapLayer.get_used_rect()
+			
+			if used_rect.has_point(click_map_pos):
+				print("Click was INSIDE the used tilemap area!")
