dde9f5c953d7af2583d317bf3031659fb68f9b94
Sharzil <sharzil362@gmail.com>
2025-11-09 09:04:40 -0600

    removed all unecessary things from nodeclass and added arrays to model class

--- model.gd
+++ model.gd
@@ -1,16 +1,16 @@
 extends nodeclass
 class_name Model
 var tile_map_layer_from_model: Node2D 
+var X_array = []
+var O_array = []
 
-func _ready() -> void:
-	tile_map_layer_from_model = tile_map_layer
 	
 # this class can prollyv check weather the clicked coordinate is empty or not
 # so we need a function for that 
 # I need to store all the values occupied
 #maybe I can make two arrays for X and Y
 func check_empty(coord: Vector2i):
-	
-	X_array.append(coord)
-	print(X_array)
+	if coord not in X_array or O_array:
+		X_array.append(coord)
+		print(X_array)
 	
--- nodeclass.gd
+++ nodeclass.gd
@@ -1,10 +1,12 @@
-extends Node2D
+extends Node
 class_name nodeclass
-var tile_map_layer: Node2D
-var X_array 
-var O_array 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	tile_map_layer = $TileMapLayer
-	X_array = []
-	O_array = []
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
