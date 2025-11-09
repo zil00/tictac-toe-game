439d19d6a6d221bac34bcacdc08b59dbf6403037
Sharzil <sharzil362@gmail.com>
2025-11-09 09:17:53 -0600

    added some statements to identify who's turn it is to play

--- model.gd
+++ model.gd
@@ -3,7 +3,7 @@ class_name Model
 var tile_map_layer_from_model: Node2D 
 var X_array = []
 var O_array = []
-
+var turn_decider = -1 #if 0 then O if 1 then X
 	
 # this class can prollyv check weather the clicked coordinate is empty or not
 # so we need a function for that 
@@ -12,5 +12,13 @@ var O_array = []
 func check_empty(coord: Vector2i):
 	if coord not in X_array or O_array:
 		X_array.append(coord)
-		print(X_array)
+		#if the space has not been occupied then we change the decider
+		turn_decider+=1
+		if turn_decider%2 == 0:
+			print("'O's' turn")
+		else:
+			print("'X's' turn") 
+		#print(X_array)
+	else:
+		print("space occupied !! Try Again !!")
 	
